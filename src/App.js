cd1ebca56c9675616efeaf5310ea7e3940a154e8
Supriya Sinha <ssinha@symplr.com>
2020-05-08 06:28:17 -0700

    es lint added

--- src/App.js
+++ src/App.js
@@ -1,121 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Menu from './Menu';
-import Home from './Home';
-import HomeMenu from './HomeMenu';
-import AboutMenu from './AboutMenu';
-import HorizontalMenu from './NavBar';
-import ContactMenu from './ContactMenu';
-import Table from './Table';
-import Movies from './Movies'
-import movieData from './data/top5MoviesAssessement.json'
-var menuItems = [1, 2, 3, 5, 6];
-var information = ['Home', 'About', 'Contact', 'signUp']
+import React from "react";
+import "./App.css";
+import HomeMenu from "./HomeMenu";
+import AboutMenu from "./AboutMenu";
+import HorizontalMenu from "./NavBar";
+import ContactMenu from "./ContactMenu";
+import Table from "./Table";
+import Movies from "./Movies";
+import movieData from "./data/top5MoviesAssessement.json";
 
 class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-                tableDatas: [],
-                showComponent: '',
-                movieData: movieData,
-                imageDatToBeSent: '',
+  constructor() {
+    super();
+    this.state = {
+      tableDatas: [],
+      showComponent: "",
+      movieData,
+      imageDatToBeSent: "",
+    };
+  }
 
+  componentDidMount() {
+    this.setState({ movieData });
+  }
 
-            }
-            // this.setState({movies:[movieData]})
-        console.log(this.state.movieData)
+  componentDidUpdate() {
+    console.error("APP UPDATED");
+    if (this.state.showComponent !== "contact")
+      this.setState({ showComponent: "contact" });
+  }
+
+  render() {
+    const onAboutMenuClick = () => {
+      // this.setState({ aboutMenu: "welocome ot about" });
+    };
+
+    const onClickTable = (item) => {
+      let newState;
+      for (let i = 0; i < this.state.tableDatas.length; i += 1) {
+        if (this.state.tableDatas[i].id === item.id - 1) {
+          newState = this.state.tableDatas;
+          newState.splice(i + 1, 1);
+        }
+      }
+      this.setState({ tableDatas: newState });
+    };
+
+    const onClickOfImage = (item) => {
+      console.log(item);
+      this.setState({ imageDatToBeSent: item });
+    };
+
+    let MainComponent = null;
+    switch (this.state.showComponent) {
+      case "Home":
+        MainComponent = <HomeMenu data="HomeMenu page loaded" />;
+        break;
+      case "About":
+        MainComponent = (
+          <AboutMenu data="AboutMenu page loaded" onClick={onAboutMenuClick} />
+        );
+        break;
+      case "Contact":
+        MainComponent = <ContactMenu data="ContactMenu page loaded" />;
+        break;
+      default:
+        MainComponent = null;
+        break;
     }
-
-
-    componentDidMount() {
-        //   console.error('APP MOUNTED')
-        //   fetch('https://jsonplaceholder.typicode.com/todos')
-        //   .then(response => response.json())
-        //  .then(json => {console.log(json)
-        //   this.setState({tableDatas:json})})      
-        console.log(this.state.tableDatas);
-        this.setState({ movieData: movieData })
-        console.log(this.state.movieData);
+    if (this.state.tableDatas) {
+      return (
+        <div className="App">
+          <div className="tilteApp">
+            <b> MY SUPER YOUTUBE APP</b>
+          </div>{" "}
+          {/* <Menu data={this.state.menuItems} onClick={onMenuClick} />
+                           <Home title={this.state.title}/> */}
+          <Movies data={this.state.movieData} onClickOfImage={onClickOfImage} />
+          <HorizontalMenu data={this.state.imageDatToBeSent} /> {MainComponent}
+          <Table data={this.state.tableDatas} onClickTable={onClickTable} />
+        </div>
+      );
     }
-
-    componentDidUpdate() {
-        console.error('APP UPDATED')
-        if (this.state.showComponent !== 'contact')
-            this.setState({ showComponent: 'contact' })
-    }
-
-    render() {
-
-        //   if (!this.state.tableDatas){
-        //     return <div>Loading...</div>
-        // }
-        const onHomeMenuClick = (item) => {
-            console.log(item)
-            this.setState({ showComponent: item })
-        }
-        const onAboutMenuClick = () => {
-            this.setState({ aboutMenu: "welocome ot about" });
-        }
-
-        const onClickTable = (item) => {
-            console.log(item);
-            for (let i = 0; i < this.state.tableDatas.length; i++) {
-                if (this.state.tableDatas[i].id === (item.id - 1)) {
-                    var newState = this.state.tableDatas
-                    newState.splice(i + 1, 1);
-                }
-            }
-            this.setState({ tableDatas: newState })
-        }
-
-        const onClickOfImage = (item) => {
-            console.log(item);
-            this.setState({imageDatToBeSent : item})
-        }
-
-        const onMoviesClick = () => {
-            console.log("inot movie details")
-        }
-
-        const displayQuestion = () => {
-            this.setState({
-                displayQuestions: !this.state.displayQuestions
-            })
-        }
-        let MainComponent = null
-        switch (this.state.showComponent) {
-            case 'Home':
-                MainComponent = < HomeMenu data = { 'HomeMenu page loaded' }
-                />
-                break;
-            case 'About':
-                MainComponent = < AboutMenu data = { 'AboutMenu page loaded' }
-                onClick = { onAboutMenuClick }
-                />
-                break;
-            case 'Contact':
-                MainComponent = < ContactMenu data = { 'ContactMenu page loaded' }
-                />
-                break;
-        }
-        if (this.state.tableDatas) {
-            return (
-                <div className = "App">
-                <div > APP TITLE </div> {
-                    /* <Menu data={this.state.menuItems} onClick={onMenuClick} />
-                           <Home title={this.state.title}/> */
-                }
-                <Movies data = { this.state.movieData } onClickOfImage = { onClickOfImage }/> 
-                <HorizontalMenu data = {this.state.imageDatToBeSent } /> { MainComponent } 
-                <Table data = { this.state.tableDatas } onClickTable = { onClickTable } />
-                
-                 </div>
-            );
-        }
-    }
-
+    return null;
+  }
 }
 
 export default App;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,25 +1,22 @@
-import React from 'react';
+import React from "react";
 
+const HorizontalMenu = (props) => {
+  const style1 = {
+    height: 500,
+    background: "#111111",
+  };
 
-const HorizontalMenu = (props)=> {
-  const style1={
-    height:500,
-    background: "#111111"
-  }
-
-    console.log(props.data)
-    return (
-      <div className="container" style={style1}>
-        <div className="carousel">
-          <div className="synopsis">{props.data.title}
-          <img className="imageResize" src={props.data.imageUrl}/>
-          </div>
-          <div className="synopsis">{props.data.synopsis}</div>
-        
+  return (
+    <div className="container" style={style1}>
+      <div className="carousel">
+        <div className="title">
+          <b>{props.data.title}</b>
+          <img className="imageResize" src={props.data.imageUrl} />
         </div>
+        <div className="synopsis">{props.data.synopsis}</div>
+      </div>
     </div>
-    ); 
-  }
-  
-  export default HorizontalMenu;
-  
+  );
+};
+
+export default HorizontalMenu;
